fb16ac0c37af5cb321d1ecff8624d73d601c3cd3
David Pereira <d_pereira@outlook.com.br>
2020-02-02 03:12:24 -0300

    Initial check-in of module CSharpProfessional

---
+++ Teste/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CantChange(new AValueType());
+        }
+
+        static void TesteParams(params int[] args)
+        {
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void CantChange(in AValueType a)
+        {
+            a.Data = 45;
+
+            Console.WriteLine(a.Data);
+        }
+
+    }
+ 
+}
